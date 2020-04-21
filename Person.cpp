c5eff3786f0d09a1d119cf9eaf83faecd8fb8738
Dmytro Buhaiov <59365256+BuhaiovDmytro@users.noreply.github.com>
2020-04-21 19:25:07 +0300

    Add files via upload

---
+++ Person.cpp
@@ -0,0 +1,18 @@
+//
+// Created by dmytro on 20/04/2020.
+//
+
+#include "Person.h"
+
+int Person::GetAge() {
+    return age;
+}
+
+std::string Person::Get_f_name() {
+    return f_name;
+}
+
+std::string Person::Get_l_name() {
+    return l_name;
+}
+
